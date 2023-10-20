9a0ee07d9d99b3cfacc791795ff6d4cc4684250f
Daniel Im <imdanieljae@gmail.com>
2023-10-20 14:28:58 -0700

    Refactoring main page of website

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,26 +4,33 @@ import { AiFillLinkedin } from "react-icons/ai";
 import { BiLogoGmail } from "react-icons/bi";
 import { HiExternalLink } from "react-icons/hi"
 
-import AnimatedCursor from "react-animated-cursor"
-
+/* STATIC IMGS */
 import scrollTo from "gatsby-plugin-smoothscroll";
+import testImg from "../images/icon.png"
 import secretH from "../images/secret-h.png"
 import pathfinder from "../images/pathfinder.png"
 import spotluck from "../images/spotluck.png"
 
+/* GIFS */
+import secretHGif from "../images/secret-h.gif"
+import spotluckGif from "../images/spotluck.gif"
+import pathfinderGif from "../images/pathfinder.gif"
+
 import "../pages/css/main.css"
+import "../images/noise.png"
 
 const pageStyle = {
   display: "flex",
   alignitems: "center",
   justifyContent: "start",
-  backgroundColor: "#121212",
   width: "100vw",
   height: "100vh",
   margin: "0",
   padding: "0",
   fontFamily: "Verdana, sans-serif",
-  lineHeight: "1.5"
+  lineHeight: "1.5",
+  background: "#121212",
+  backgroundImage: "url('src\images\noise.png')"
 };
 
 /**
@@ -33,12 +40,12 @@ const pageStyle = {
 const sideHeader = {
   position: "fixed",
   display: "flex",
-  backgroundColor: "#121212",
   height: "100vh",
   width: "50vw",
   alignSelf: "self-start",
   justifyContent: "start",
   flexDirection: "column",
+  background: "#121212"
 };
 
 const sideHeaderBringRight = {
@@ -80,27 +87,24 @@ const subTitleStyle = {
 };
 
 const descriptionTitleStyle = {
-  position: "relative",
   alignSelf: "start",
-  fontSize: "1rem",
+  fontSize: ".8rem",
   // border: "1px solid black",
   fontFamily: "Verdana, sans-serif",
-  margin: "15px 0px 5vh 7px",
+  margin: "15px 0px 0px 7px",
   color: "#ADADAD",
   lineHeight: "1.3"
 };
 
 const linkContainer = {
-  position: "relative",
   fontSize: "2.5em",
   flexDirection: "row",
   justifyContent: "space-between",
   alignSelf: "start",
   height: "12vh",
   width: "60%",
-  margin: "10vh 5vh 0px 5vh",
-  color: "#ADADAD",
-  top: "20vh"
+  margin: "10vh 5vh 0 5vh",
+  color: "#ADADAD"
 };
 
 const linkStyle = {
@@ -159,8 +163,8 @@ const scrollSectionBringLeft = {
 const originStyle = {
   // border: "1px solid white",
   width: "450px",
-  height: "450px",
-  margin: "5vh 0 0 25px",
+  height: "auto",
+  margin: "5.5vh 0 0 25px",
   color: "#999999",
   fontSize: "14px",
   fontWeight: "400",
@@ -233,11 +237,12 @@ const projectListContainer = {
   display: "flex",
   flexDirection: "column",
   width: "450px",
-  height: "600px",
-  margin: "0vh 0 0 25px",
+  height: "auto",
+  margin: "3vh 0 0 25px",
 };
 
 const projectStyle = {
+    position: "relative",
     display: "flex",
     width: "550px",
     height: "auto",
@@ -256,7 +261,22 @@ const projectPhotoHeader = {
     fontWeight: "500",
     color: "#B8B8B8",
     border: "1px solid #B8B8B8",
-    objectFit: "cover"
+    objectFit: "cover",
+    zIndex: '1'
+};
+
+const projectGifHeader = {
+    position: "absolute",
+    height: "100px",
+    borderRadius: "5%",
+    width: "22%",
+    left: "5px",
+    top: "14px",
+    fontWeight: "500",
+    color: "#B8B8B8",
+    border: "1px solid #B8B8B8",
+    objectFit: "cover",
+    zIndex: '0'
 };
 
 const projectCard = {
@@ -272,6 +292,11 @@ const projectTitle = {
     fontWeight: "500",
     // color: "#EBEBEB"
 };
+
+const projectIntroDiv = {
+    fontSize: "1rem",
+    color: "white"
+}
 
 const projectDescription = {
   position: "relative",
@@ -305,85 +330,61 @@ const resumeLinkStyle = {
     textDecoration: "none"
 }
 
-const projectLinkStyle = {
-  position: "relative",
-  fontWeight: "14px",
-  color: "white",
-  textDecoration: "none"
-}
-
 const boldTextStyle = {
     color: "#EBEBEB",
     fontWeight: "550",
     textDecoration: "none"
 }
 
-
 const IndexPage = () => {
   return (
     <main style={pageStyle}>
-    <AnimatedCursor 
-      color="220, 90, 90"
-      innerSize={10}
-      outerSize={45}
-      innerScale={1}
-      outerScale={1.9}
-      outerAlpha={1}
-      outerStyle={{
-        mixBlendMode: 'exclusion'
-      }}
-    />
       <div style={sideHeader}>
         <div style={sideHeaderBringRight}>
           <div style={titleContainer}>
             <div style={titleStyle}> Daniel Im </div>
             <div style={subTitleStyle}> Software Engineer </div>
             <div style={descriptionTitleStyle}>
-              {" "}
-              I build engaging and thoughtful digital experiences for the web.{" "}
+              <li><a href="https://aws.amazon.com/" target="_blank">Amazon AWS</a> Software Engineer (2 years)</li>
+              <li>CS Graduate @ <a href="https://eecs.berkeley.edu/academics/undergraduate/cs-ba" target="_blank">UC Berkeley</a> - Tech GPA 3.5</li>
+              <li>Fullstack Certified @ <a href="https://www.rithmschool.com/" target="_blank">Rithm School</a></li>
+              <br></br>
+              <HiExternalLink/>
+                <a id="resumeLink" target="_blank" style={resumeLinkStyle} href="https://drive.google.com/file/d/1a3-2168ndwbbACzztowBK85TLkevTGpa/view?usp=sharing">Link to my resume</a>
+              <br></br>
+              <br></br>
+
             </div>
           </div>
           <div style={linkContainer}>
-            <div
-              id="pageLink" 
-              onClick={() => scrollTo("#scroll-about")}
-              style={linkStyle}>
+            <div onClick={() => scrollTo("#scroll-about")} style={linkStyle}>
               {" "}
               ABOUT
             </div>
             <div
-              id="pageLink" 
               onClick={() => scrollTo("#scroll-experience")}
               style={linkStyle}
             >
               {" "}
               EXPERIENCE
             </div>
-            <div 
-              id="pageLink" 
-              onClick={() => scrollTo("#scroll-projects")}
-              style={linkStyle}>
+            <div onClick={() => scrollTo("#scroll-projects")} style={linkStyle}>
               {" "}
               PROJECTS
             </div>
           </div>
           <div style={socialsContainer}>
             <div style={socialStyle}>
-              <a target="_blank" href="https://github.com/danieljaeim">
-                <AiFillGithub />
-              </a>
+              {" "}
+              <AiFillGithub />{" "}
             </div>
             <div style={socialStyle}>
               {" "}
-              <a target="_blank" href="https://www.linkedin.com/in/imdanieljae/">
-                <AiFillLinkedin />{" "}
-              </a>
+              <AiFillLinkedin />{" "}
             </div>
             <div style={socialStyle}>
               {" "}
-              <a target="_blank" href="mailto:imdanieljae@gmail.com">
-                <BiLogoGmail />{" "}
-              </a>
+              <BiLogoGmail />{" "}
             </div>
           </div>
         </div>
@@ -392,35 +393,95 @@ const IndexPage = () => {
         <div style={scrollSectionBringLeft}>
           <div style={originStyle}>
             Back in 2017, I tried my hand at creating a game in Adobe Flash and
-            fell headfirst into the world of programming and web development. Over the next few years, I dedicated myself to learn programming by completing a <b style={boldTextStyle}>fullstack
-            bootcamp</b> in San Francisco, finishing my <b style={boldTextStyle}>bachelors at UC Berkeley</b>, and working at <b style={boldTextStyle}>Amazon as a
-            software engineer</b> for two years.
+            fell headfirst into the world of programming.
             <br></br>
-            <br></br>
-            Now, I am searching for my next professional opportunity, specifically
-            for <b style={boldTextStyle}>fullstack development roles</b> based in Seattle.
-            <br></br>
+            {/* <br></br> */}
+            {/* Now, I am searching for my next professional opportunity, specifically
+            for <b style={boldTextStyle}>fullstack development roles</b> based in Seattle. */}
+            {/* <br></br> */}
             <br></br>
             My main focus these days is building products that meld my passion
-            for games and web development. In my freetime, I've released a
-            <b style={boldTextStyle}> Discord Bot</b> that been downloaded in 1000+ servers, 
-            a <b style={boldTextStyle}>browser application</b> for graphing album charts,
-            and a website that <b style={boldTextStyle}> guesses which Magic the Gathering</b> card you're thinking of.
+            for games, music, and the internet. 
             <br></br>
             <br></br>
-            When I'm not on my computer, I'm lifting rocks at the gym, hosting
-            hot-pot at my apartment, or releasing music on <b style={boldTextStyle} > <a target="_blank" style={experienceLink} href="https://soundcloud.com/dufe-258062613">my Soundcloud</a></b> (this is definitely a plug).
+            Come check out some of my projects below, and if you like what you see, reach out via email -  
+            <a target="_blank" href="mailto:imdanieljae@gmail.com">
+                imdanieljae@gmail.com
+            </a>
             <br></br>
-            <br></br>I am actively seeking work opportunities. Here is my email:
+            <br></br>I am actively seeking work opportunities!
             <br></br>
             <br></br>
-            imdanieljae@gmail.com
-            <br></br>
-            <br></br>
-            <HiExternalLink/>
-            <a id="resumeLink" target="_blank" style={resumeLinkStyle} href="https://drive.google.com/file/d/1a3-2168ndwbbACzztowBK85TLkevTGpa/view?usp=sharing">Link to my resume</a>
+            </div>
+          <div id="scroll-projects" style={projectListContainer}>
+            {/* <div style={projectIntroDiv}> Here are some projects I am really proud of: </div> */}
+            <div className="projectStyle" style={projectStyle}>
+              <img id="projectPhotoHover" src={secretH} style={projectPhotoHeader}/>
+              <img src={secretHGif} style={projectGifHeader}/>
+              <div style={projectCard}>
+                <div id="experienceTitle" style={projectTitle}> Secret H. Discord Bot </div>
+                <div style={projectDescription}>
+                  <br></br>
+                  A Discord app built during COVID that automatically
+                  moderates games of Secret Hitler over Discord. Unmask traitors,
+                  forge alliances, and ignite chaos in this intense social deduction game.
+                  <br></br>
+                  Now in 100+ servers.
+                  <br></br>
+                  <div style={tagContainer}>
+                    <span style={tagStyle}>React</span>
+                    <span style={tagStyle}>Node.js</span>
+                    <span style={tagStyle}>Discord API</span>
+                    <span style={tagStyle}>Web Hosting</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="projectStyle" style={projectStyle}>
+              <img id="projectPhotoHover" src={spotluck} style={projectPhotoHeader}/>
+              <img src={spotluckGif} style={projectGifHeader}/>
+              <div style={projectCard}>
+                <div id="experienceTitle" style={projectTitle}>
+                    Spotluck - Music Recommender
+                </div>
+                <div style={projectDescription}>
+                  <br></br>
+                  Browser application that leverages Spotify's SDK to recommend new 
+                  music based on top your favorite artists, and various sonic 
+                  qualities.
+                  <br></br>
+                  <div style={tagContainer}>
+                    <span style={tagStyle}>Javascript</span>
+                    <span style={tagStyle}>Spotify API</span>
+                    <span style={tagStyle}>CSS</span>
+                    <span style={tagStyle}>Music Theory</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="projectStyle" style={projectStyle}>
+              <img id="projectPhotoHover" src={pathfinder} style={projectPhotoHeader}/>
+              <img src={pathfinderGif} style={projectGifHeader}/>
+              <div style={projectCard}>
+                <div id="experienceTitle" style={projectTitle}>
+                    Pathfinding Algorithm Visualizer
+                </div>
+                <div style={projectDescription}>
+                  <br></br>
+                  An inbrowser pathfinding visualizer for Djikstra's, A*, BFS/DFS with 
+                  the ability to add mazes.
+                  <br></br>
+                  <div style={tagContainer}>
+                    <span style={tagStyle}>React</span>
+                    <span style={tagStyle}>HTML5</span>
+                    <span style={tagStyle}>CSS</span>
+                    <span style={tagStyle}>Search Algorithms</span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div id="scroll-experience" style={experienceListContainer}>
+                    <div id="scroll-experience" style={experienceListContainer}>
             <div className="experienceStyle" style={experienceStyle}>
               <div style={experienceDateHeader}>
                 2022 - 2023
@@ -472,11 +533,10 @@ const IndexPage = () => {
                     Led monthly modules for peer instructors to learn React and web development.
                     <br></br>
                     <div style={tagContainer}>
-                        <span style={tagStyle}>Excel</span>
+                        <span style={tagStyle}>React</span>
                         <span style={tagStyle}>Python</span>
-                        <span style={tagStyle}>HTML5</span>
+                        <span style={tagStyle}>CSS</span>
                         <span style={tagStyle}>Javascript</span>
-                        <span style={tagStyle}>Powerpoint Presentations</span>
                     </div>
                     </div>
                 </a>
@@ -510,72 +570,7 @@ const IndexPage = () => {
               </div>
             </div>
           </div>
-          <div id="scroll-projects" style={projectListContainer}>
-            <div className="projectStyle" style={projectStyle}>
-              <img src={secretH} style={projectPhotoHeader}/>
-              <div style={projectCard}>
-                <a style={experienceLink} target="_blank" href="https://discord.com/api/oauth2/authorize?client_id=1133129369792020640&permissions=326417774672&scope=bot">
-                  <div id="experienceTitle" style={projectTitle}> Secret H. Discord Bot </div>
-                  <div style={projectDescription}>
-                    <br></br>
-                    A Discord app built during COVID that automatically
-                    moderates games of Secret Hitler over Discord. Unmask traitors,
-                    forge alliances, and ignite chaos in this intense social deduction game.
-                    <br></br>
-                    Now in 100+ servers.
-                    <br></br>
-                    <div style={tagContainer}>
-                      <span style={tagStyle}>Node.js</span>
-                      <span style={tagStyle}>Discord API</span>
-                      <span style={tagStyle}>Web Hosting</span>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div className="projectStyle" style={projectStyle}>
-              <img src={spotluck} style={projectPhotoHeader}/>
-              <div style={projectCard}>
-                <a style={experienceLink} target="_blank" href="https://www.google.com">
-                  <div id="experienceTitle" style={projectTitle}> Spotluck </div>
-                  <div style={projectDescription}>
-                    <br></br>
-                    A music recommendation app and inbrowser Spotify player, that recommends
-                    new songs based on your top 100 most listened artists. Leverages the Spotify API
-                    and built with React.
-                    <br></br>
-                    <div style={tagContainer}>
-                      <span style={tagStyle}>React</span>
-                      <span style={tagStyle}>Node.js</span>
-                      <span style={tagStyle}>Spotify SDK</span>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div className="projectStyle" style={projectStyle}>
-              <img src={pathfinder} style={projectPhotoHeader}/>
-              <div style={projectCard}>
-                <a style={experienceLink} target="_blank" href="https://www.google.com">
-                  <div id="experienceTitle" style={projectTitle}>
-                      Pathfinding Algorithm Visualizer
-                  </div>
-                  <div style={projectDescription}>
-                    <br></br>
-                    An inbrowser pathfinding visualizer for Djikstra's, A*, BFS/DFS with 
-                    the ability to add mazes.
-                    <br></br>
-                    <div style={tagContainer}>
-                      <span style={tagStyle}>React</span>
-                      <span style={tagStyle}>HTML5</span>
-                      <span style={tagStyle}>CSS</span>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-          </div>
-          </div>
+        </div>
       </div>
     </main>
   );
@@ -585,14 +580,13 @@ export default IndexPage;
 
 const body = {
   backgroundColor: "#121212",
-  overflowX: "hidden"
 };
 
 export const Head = () => {
   return (
     <>
       <html lang="en" />
-      <title> 🇩  Daniel Jae Im</title>
+      <title>Daniel Jae Im</title>
       <meta name="description" content="Hello World" />
       <body style={body} />
     </>
